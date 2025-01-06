5f14571722c27184af27d278dff4abcf5d1eb75c
Sebastian Navarro <sebastian.navarro.saavedra@gmail.com>
2025-01-05 19:59:47 -0800

    Adding linkedlist examples

--- examples/04_03_ds_linkedlist.rs
+++ examples/04_03_ds_linkedlist.rs
@@ -1,1 +1,19 @@
-fn main(){}
+use std::collections::LinkedList;
+fn main(){
+    let mut ll = LinkedList::new();
+    ll.push_back('a');
+    ll.push_back('b');
+    ll.push_back('c');
+    ll.push_front('0');
+
+    println!("{:?}", ll);
+
+    ll.pop_front();
+    ll.pop_back();
+
+    println!("{:?}", ll);
+
+    // ll.clear() remove all elements from the linked list
+
+    // contains() if a element exist in the linked list 
+}
